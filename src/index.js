1d04d6be161e56a8dbb7a1201921f4b528b8073c
AlexisNarvaez <alexisnarvaez97@hotmail.com>
2020-04-18 19:00:11 -0500

    Peticiones basicas de un crud con fetch

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 // import { init } from "./js/chistes-page";
-import { obtenerUsuarios } from "./js/http-provider";
-import { initUsuarios } from "./js/usuarios-page";
+// import { obtenerUsuarios } from "./js/http-provider";
+// import { initUsuarios } from "./js/usuarios-page";
+
+import * as CRUD from './js/crud';
+
+
+// CRUD.getUsuario(2).then( console.log);
+
+// CRUD.crearUsuario({
+//     name: 'Alexis',
+//     job: 'Developer'
+// }).then( console.log);
+
+CRUD.actualizarUsuario(1, {
+    name: 'Gerardo',
+    job: 'Sock'
+}).then(console.log)
+
+CRUD.borrarUsuario(1).then( console.log );
 
 // init();
 
-initUsuarios();
+// initUsuarios();
 
 // obtenerUsuarios().then( console.log);
